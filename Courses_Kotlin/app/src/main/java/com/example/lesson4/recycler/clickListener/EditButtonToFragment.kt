e6d5ae3ec77e9210181c01d608f4cf57fbaf5ee0
Relaxiy <terer435461@gmail.com>
2022-02-24 00:39:26 +0300

    lesson4

--- Courses_Kotlin/app/src/main/java/com/example/lesson4/recycler/clickListener/EditButtonToFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lesson4.recycler.clickListener
-
-import android.widget.EditText
-
-interface EditButtonToFragment {
-    fun getButton() : EditText
-}
--- Courses_Kotlin/app/src/main/java/com/example/lesson4/recycler/clickListener/TextViewOnClick.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lesson4.recycler.clickListener
-
-import com.example.lesson4.enums.Cars
-
-interface TextViewOnClick {
-    fun textViewClick(item: Cars)
-}
