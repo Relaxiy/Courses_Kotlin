02cb8e951a22b63879b39624602ab1c112db7147
Relaxiy <terer435461@gmail.com>
2022-03-03 13:12:27 +0300

    koin

--- Courses_Kotlin/lesson4/app/src/main/java/com/example/lesson4/domain/CardInteractorImpl.kt
+++ Courses_Kotlin/lesson4/app/src/main/java/com/example/lesson4/domain/CardInteractorImpl.kt
@@ -1,13 +1,13 @@
 package com.example.lesson4.domain
 
+import com.example.lesson4.domain.ext.concat
 import com.example.lesson4.domain.models.GraphicCardPresent
-import com.example.lesson4.domain.validation.CardConcat
 
 class CardInteractorImpl(val repository: CardRepository): CardInteractor {
     override fun getCards(): List<GraphicCardPresent> {
         return repository.getCards().map { graphicCardData ->
             GraphicCardPresent(
-                graphicCard = CardConcat.returnCard(graphicCardData)
+                graphicCard = graphicCardData.concat()
             )
         }
     }
